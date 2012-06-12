790363cd098ee6b6f7b88b02fa46c31a3a656cc9
Nickolas D. Jones <nick.d.g.jones@gmail.com>
2012-06-12 12:57:15 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AndroidArmory::Application.config.secret_token = 'a1cd0acf98d05c1bdc469afcad6999cdaa6fdd7cc057e285d9f55fc562001f90e4a3580349003ca734c62e944fbfe75522f24e1e05736d07adc70a7e5648a457'
